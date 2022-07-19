32deb9016cd2f9743e51ec96cc21eb8e5b6e3505
ankitbansal015 <ankitbansal015@gmail.com>
2022-07-19 13:08:45 +0530

    changes

---
+++ 01-hello world/src/helloworld.java
@@ -0,0 +1,13 @@
+
+public class helloworld {
+
+	public static void main(String[] args) {
+		System.out.println("hello world");
+		greet("Ankit");
+	}
+
+	public static void greet(String name) {
+		System.out.println("Hello" + name);
+	}
+	
+}
