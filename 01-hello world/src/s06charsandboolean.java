9cb5587387425b0a7298c64d293fe9f7564638d5
ankitbansal015 <ankitbansal015@gmail.com>
2022-07-19 22:34:41 +0530

    changes

---
+++ 01-hello world/src/s06charsandboolean.java
@@ -0,0 +1,29 @@
+public class s06charsandboolean {
+
+	public static void main(String[] args) {
+		
+		char myChar = 'D';
+		System.out.println("MyChar: " + myChar);
+		
+		int i = myChar;
+		System.out.println(i);
+		
+		char x = 69;
+		System.out.println("Char x: " + x);
+		
+		char mynewchar = '\u0044';
+		System.out.println(mynewchar);
+		
+		
+		mynewchar = '\u00A9';
+		System.out.println(mynewchar);
+		
+		boolean isTrue = true;
+		System.out.println(isTrue);
+		
+				
+		
+		
+	}
+	
+}
